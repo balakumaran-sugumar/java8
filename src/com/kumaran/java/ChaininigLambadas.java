e6f4b64e3230de90c12a17f04cd81bcb03ab3f72
Balakumaran <bsukumar@ups.com>
2018-09-03 18:38:10 -0400

    Refactoring the code to have Java 8

---
+++ src/com/kumaran/java/ChaininigLambadas.java
@@ -0,0 +1,29 @@
+package com.kumaran.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ChaininigLambadas {
+
+    public static void main(String ... args){
+
+        List<String> list1 = Arrays.asList("One", "two", "three", "four", "five");
+
+        List<String> list2 = new ArrayList<>();
+
+        Consumer<String> c1 = System.out::println;
+        //c1.accept("**** First List : list1 : " + list1.toString() + " ****");
+
+        Consumer<String> c2 = list2::add;
+
+        //adding the contents of list1 to list2
+        list1.forEach(c1.andThen(c2));
+
+        //Print the second list
+        c1.accept("Second list : list2 : " + list2.toString() + " ****");
+
+
+    }
+}
